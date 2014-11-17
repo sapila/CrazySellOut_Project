8481c1b38cec2c1fdd1834e5a613cde5f4e49e55
AlcatrazGR <alcatrazgr0@gmail.com>
2014-11-17 19:42:40 +0200

    Olokrirothikan 2 sinartiseis sto OffersDAO
    Egrapsa twn kwdika gia tis duo synartiseis ths offersDAO pou kaloun ton
    kwdika ths offerDataWriter kai offerDataReader.  Sthn synartisi
    getofferList kalw mia synartisi pou pros to paron dn yparxei auth tha
    epistrefei ton arithmo ton grammwn tou text gia na exei kapio orio h
    parakto for.

--- CrazySellOut/src/com/example/crazysellout/OffersDAO.java
+++ CrazySellOut/src/com/example/crazysellout/OffersDAO.java
@@ -1,8 +1,11 @@
 package com.example.crazysellout;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.example.crazysellout.Interfaces.IOffersDAO;
+import com.example.crazysellout.UserSide.OfferDataReader;
+import com.example.crazysellout.storeside.OfferDataWriter;
 
 //Class which communicates with the file (AT THE MOMENT TXT FILE) that contains the data.
 public class OffersDAO implements IOffersDAO {
@@ -15,14 +18,27 @@ public class OffersDAO implements IOffersDAO {
 
 	@Override
 	public List<Offer> getOffersList() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Offer> offerList = new ArrayList<Offer>();
+		OfferDataReader odr = new OfferDataReader();
+		Offer offerData = new Offer();
+		
+		//The method that is called will be made later
+		odr.getNumberOfRows();
+		
+		int rowNumber = odr.DataRowNumbers;
+		
+		for(int i=0; i<rowNumber; i++){
+			offerData = odr.ReadOffer();
+			offerList.add(offerData);
+		}
+		
+		return offerList;
 	}
 
 	@Override
 	public void addOfferToDAO(Offer newOffer) {
-		// TODO Auto-generated method stub
-		
+		OfferDataWriter odw = new OfferDataWriter();
+		odw.addOffer(newOffer);
 	}
 
 }
--- CrazySellOut/src/com/example/crazysellout/UserSide/OfferDataReader.java
+++ CrazySellOut/src/com/example/crazysellout/UserSide/OfferDataReader.java
@@ -4,10 +4,11 @@ import com.example.crazysellout.Offer;
 
 //Class that, reads the file of the data.
 public class OfferDataReader {
+	public int DataRowNumbers;
 	
 	//Returns a single offer (a single line from the data file) with the help 
 	//of the indexLine variable.
-	Offer ReadOffer(int indexLine){
+	public Offer ReadOffer(){
 
 		return null;
 	}
