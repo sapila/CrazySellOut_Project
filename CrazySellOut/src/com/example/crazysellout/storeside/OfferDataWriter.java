9d4269c5d327879054545faec7f0424cdbd31d59
anyaman <anastasia.dsa@hotmail.com>
2014-11-20 21:00:53 +0200

    werqwe

--- CrazySellOut/src/com/example/crazysellout/storeside/OfferDataWriter.java
+++ CrazySellOut/src/com/example/crazysellout/storeside/OfferDataWriter.java
@@ -1,12 +1,26 @@
 package com.example.crazysellout.storeside;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 import com.example.crazysellout.Offer;
 
 public class OfferDataWriter {
 	
 	public void addOffer(Offer newOffer)
 	{
-		
+		try{
+			
+			FileOutputStream fout = new FileOutputStream("res/drawable/offers");
+			ObjectOutputStream oos = new ObjectOutputStream(fout);
+			oos.writeObject(newOffer);
+			
+			oos.close();
+		}
+		catch(Exception e){
+			
+			
+		}
 	}
 
 }
